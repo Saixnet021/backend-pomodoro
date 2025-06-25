f040e55f96775d48b91a534e3d7aa64b648dfb9e
saixnet <fernandezanderson562@gmail.com>
2025-06-24 19:26:32 -0500

    gitactualizado

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { createSpotifyApi } = require('./spotify');
+const { createSpotifyApi } = require('../spotify');
 
 dotenv.config();
 
